cd953d27f478f95554fc929202f94e98c4e4fec2
Bruno Penner <brunopenner01@gmail.com>
2024-11-07 16:55:22 +0800

    #649. When Does React Re-Render?

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,9 +1,13 @@
 import {useState} from "react";
 
 export default function Counter() {
+    console.log("RE-RENDERED!!!");
     const [count, setCount] = useState(0);
     const addOne = () => {
         setCount(count+1)
+    };
+    const setToTen = () => {
+        setCount(10)
     };
     const addThree = () => {
         setCount(c => c+1)
@@ -15,6 +19,8 @@ export default function Counter() {
             <p>Count: {count}</p>
             <button onClick={addOne}>+1</button>
             <button onClick={addThree}>+3</button>
+            
+            <button onClick={setToTen}>Set to Ten</button>
         </div>
     )
 }
