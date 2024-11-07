8eccad803640ae06943157b83068e25d1759c591
Bruno Penner <brunopenner01@gmail.com>
2024-11-07 09:20:44 +0800

    #648. State Initializer Functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './Counter'
+import Dumbo from './Dumbo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
         <h1>State Demo</h1>
         <Counter />
+        <Dumbo />
     </>
   )
 }
