1e71584af5ab2a8d5f634e55e31fdf8f47f9d62e
Bruno Penner <brunopenner01@gmail.com>
2024-11-12 07:44:40 +0800

    #653. Deleting From Arrays The React Way

--- src/EmojiClicker.jsx
+++ src/EmojiClicker.jsx
@@ -1,13 +1,27 @@
-import {useState} from "react"
+import {v4 as uuid } from "uuid";
+import {useState} from "react";
+
+function randomEmoji() {
+    const choices = ["😍", "😁", "😢", "😒", "😎", "😘"];
+    return choices [Math.floor(Math.random()*choices.length)];
+}
+
 export default function EmojiClicker() {
-    const [emojis, setEmojis] = useState([{id: uuid(), emoji:"😭"}]);
+    const [emojis, setEmojis] = useState([{id: uuid(), emoji: randomEmoji()}]);
     const addEmoji = () => {
-        setEmojis(oldEmojis => [...oldEmojis, {id: uuid(), emoji:"😘"}]);
+        setEmojis(oldEmojis => [...oldEmojis, {id: uuid(), emoji:randomEmoji()}]);
+    }
+
+    const deleteEmoji = (id) => {
+        setEmojis(prevEmojis => {
+            return prevEmojis.filter(e => e.id !== id)
+        })
+        
     }
     return (
         <div>
             {emojis.map((e) => (
-                <span key={e.id} style={{fontSize: "4rem"}}>{e.emoji}</span>
+                <span onClick={() => deleteEmoji(e.id)} key={e.id} style={{fontSize: "4rem"}}>{e.emoji}</span>
             ))}
             <button onClick={addEmoji}>Add Emoji</button>
         </div>
